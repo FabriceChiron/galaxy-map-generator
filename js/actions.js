64d9cb6d1976069dfc8d0ef23167e7f2d26298d5
FabriceChironPbx <fabrice.chiron@photobox.com>
2019-05-09 14:24:47 +0200

    Adapt code to link position change

--- js/actions.js
+++ js/actions.js
@@ -24,7 +24,7 @@ const createActiveAstralBody = (astralBody, stellarSystem) => {
   activeAstralBody.id += 'Active';
   activeAstralBody.className += ' active';
 
-  activeAstralBody.querySelector('.astralBody').href = stripHref(activeAstralBody.querySelector('.astralBody'));
+  activeAstralBody.querySelector('.hover-area').href = stripHref(activeAstralBody.querySelector('.hover-area'));
 
   // stripHref(activeAstralBody.querySelector('.astralBody').href);
 
